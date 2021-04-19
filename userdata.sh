b0ef39eb2a1c886f5b61bb43aa966ad6cfff111c
SaeedKhan999 <44093072+SaeedKhan999@users.noreply.github.com>
2021-04-19 15:48:10 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-sudo apt update 
-sudo apt install -y apache2
-sudo service apache2 start
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt update -y
+apt install unzip apt-transport-https -y
+apt-get install elasticsearch -y
+sudo echo "network.host: localhost" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo echo "http.port: 9200" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo echo "xpack.security.enabled: true" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo echo "discovery.type: single-node" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+
+systemctl start elasticsearch
+systemctl enable elasticsearch
+
+printf 'y' | /usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto
+service elasticsearch stop
+
+printf '\n\n' | /usr/share/elasticsearch/bin/elasticsearch-certutil  ca
+printf '\n\n' | /usr/share/elasticsearch/bin/elasticsearch-certutil cert
+sudo cp /usr/share/elasticsearch/elastic-certificates.p12 /etc/elasticsearch/
+printf '\n\n\n\n\n\n\n\n\n\n\n\n\n' | /usr/share/elasticsearch/bin/elasticsearch-certutil  http
+
+unzip /usr/share/elasticsearch/elasticsearch-ssl-http.zip 
+
+sudo cp /etc/elasticsearch/elasticsearch/http.p12 /etc/elasticsearch/
+sudo echo "xpack.security.http.ssl.enabled: true" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo echo "xpack.security.http.ssl.keystore.path: "http.p12"" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo echo "xpack.security.transport.ssl.enabled: true" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+printf '\n' | /usr/share/elasticsearch/bin/elasticsearch-keystore add xpack.security.http.ssl.keystore.secure_password
+
+systemctl start elasticsearch
